24e99f6d76095bcce4cd9790134bf9fab5e7ce1f
xd83 <1455817493@qq.com>
2023-01-31 00:27:17 +0800

    commit

--- CMDB/settings.py
+++ CMDB/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CMDB.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- assets/views.py
+++ assets/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from assets import models
 from assets import asset_handler
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 @csrf_exempt
@@ -30,7 +31,7 @@ def report(request):
             asset_obj = models.Asset.objects.filter(sn=sn)
             if asset_obj:
                 # 进入已上线资产的数据更新流程
-                pass
+                update_asset = asset_handler.UpdateAsset(request, asset_obj[0], data)
                 return HttpResponse("资产数据已经更新！")
             else:   # 如果已上线资产中没有，那么说明是未批准资产，进入新资产待审批区，更新或者创建资产。
                 obj = asset_handler.NewAsset(request, data)
@@ -39,3 +40,41 @@ def report(request):
         else:
             return HttpResponse("没有资产sn序列号，请检查数据！")
     return HttpResponse('200 ok')
+
+
+def index(request):
+
+    assets = models.Asset.objects.all()
+    return render(request, 'assets/index.html', locals())
+
+
+def dashboard(request):
+  total = models.Asset.objects.count()
+  upline = models.Asset.objects.filter(status=0).count()
+  offline = models.Asset.objects.filter(status=1).count()
+  unknown = models.Asset.objects.filter(status=2).count()
+  breakdown = models.Asset.objects.filter(status=3).count()
+  backup = models.Asset.objects.filter(status=4).count()
+  up_rate = round(upline / total * 100)
+  o_rate = round(offline / total * 100)
+  un_rate = round(unknown / total * 100)
+  bd_rate = round(breakdown / total * 100)
+  bu_rate = round(backup / total * 100)
+  server_number = models.Server.objects.count()
+  networkdevice_number = models.NetworkDevice.objects.count()
+  storagedevice_number = models.StorageDevice.objects.count()
+  securitydevice_number = models.SecurityDevice.objects.count()
+  software_number = models.Software.objects.count()
+
+  return render(request, 'assets/dashboard.html', locals())
+
+
+def detail(request, asset_id):
+    """
+    以显示服务器类型资产详细为例，安全设备、存储设备、网络设备等参照此例。
+    :param request:
+    :param asset_id:
+    :return:
+    """
+    asset = get_object_or_404(models.Asset, id=asset_id)
+    return render(request, 'assets/detail.html', locals())
